4a11cdec2ad588bab770150c638bb8b46de92569
99mini <itman390@gmail.com>
2025-06-17 18:41:26 +0900

    fix(example): config file use js

---
+++ examples/i18n.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@99mini/i18n-cli').I18nConfig} */
+const config = {
+  outputDir: './.i18n',
+  localesDir: 'public/_locales',
+  defaultLanguage: 'en',
+  supportedLanguages: ['en', 'ko'],
+};
+
+export default config;
